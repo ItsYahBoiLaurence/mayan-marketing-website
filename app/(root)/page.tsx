9e19b351a904b3a2d92506fca5c8b7c2737a0bc4
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-03-05 12:10:21 +0800

    NextJS & Mantine Setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Box, Paper, Text } from "@mantine/core";
+
+export default function Home() {
+  return (
+    <Box>
+      <Paper bg='jonquil' >
+        <Text ta='center'>asdasd</Text>
+      </Paper>
+    </Box>
+  );
+}
