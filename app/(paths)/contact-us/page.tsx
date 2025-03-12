015900f89aab0be1c7d3bcc3d95791d9c9cb91ef
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-03-12 19:07:50 +0800

    Contact Us Finished

--- app/(paths)/contact-us/page.tsx
+++ app/(paths)/contact-us/page.tsx
@@ -1,9 +1,31 @@
+import PageBanner from '@/app/components/Banners/PageBanner'
+import { BackgroundImage, Box, Button, Center, Flex, Group, Image, Paper, Stack, Title } from '@mantine/core'
+import Text from '@/app/components/MantineComponents/Text'
 import React from 'react'
-
+import Form from '@/app/components/Form'
 const page = () => {
     return (
         <div>
-            contact us
+            <BackgroundImage src='/contactBanner.png' w={'100%'} h={'80vh'} mb={'60px'}>
+                <Paper h={"100%"} w={"100%"} bg={'#042161'} opacity={0.95}>
+                    <Flex w={"100%"} h={'100%'} justify={'center'} align={'center'}>
+                        <Box style={{ flex: 0.5 }} h={'100%'}>
+                            <Center h={'100%'} px={'xl'}>
+                                <Stack w={'80%'}>
+                                    <Box>
+                                        <Title c={'#fff'} fw={700}>Share your Stories.</Title>
+                                        <Title c={'#fff'} fw={700}>Let's get in touch.</Title>
+                                    </Box>
+                                    <Text size='md' c={'#fff'}>Learn how to boost your business by harnessing the power of technology and discover innovative tools to drive success.</Text>
+                                </Stack>
+                            </Center>
+                        </Box>
+                        <Box style={{ flex: 0.35 }} pos={'relative'} h={'100%'}>
+                            <Form />
+                        </Box>
+                    </Flex>
+                </Paper>
+            </BackgroundImage>
         </div>
     )
 }
---
+++ app/components/Form/index.tsx
@@ -0,0 +1,29 @@
+import { BackgroundImage, Box, Button, Center, Checkbox, Paper, Stack, Text, Textarea, TextInput, Title } from '@mantine/core'
+import React from 'react'
+
+const index = () => {
+    return (
+        <BackgroundImage src='/formBg.jpeg' pos={'absolute'} bottom={'-50px'} h={'550px'} radius={'lg'}>
+            <Paper p={'xl'} radius={'lg'} w={'100%'} h={'100%'} bg={'#EFEFEF'} opacity={1} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Stack w={'90%'} h={'80%'}>
+                    <Title order={3} fw={500}>Let's talk about your business!</Title>
+                    <form>
+                        <Stack>
+                            <Stack>
+                                <TextInput placeholder='Enter your full name' label={<Text>What is your name?</Text>} />
+                                <TextInput placeholder='Enter your email' label={<Text>Email</Text>} />
+                                <Textarea placeholder='Please tell us what you are trying to achieve' label={<Text>Message</Text>} />
+                            </Stack>
+                            <Checkbox label={<Text size='xs'>By clicking the checkbox you agree to the Privacy Policy and terms of service </Text>} />
+                            <Button variant='filled' color='blue'>Submit</Button>
+                        </Stack>
+
+                    </form>
+                </Stack>
+            </Paper>
+        </BackgroundImage>
+
+    )
+}
+
+export default index
