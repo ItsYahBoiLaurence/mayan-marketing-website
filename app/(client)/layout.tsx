37646d061e1bca5d16a06e03adee6fa7cab30787
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-03-17 05:31:35 +0800

    Hotjar Script Inclusion

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -11,8 +11,20 @@ export const metadata: Metadata = {
   description: "Mayan Solutions",
   icons: {
     icon: "/logo.png"
+  },
+  other: {
+    "hotjar-script": `(function (c, s, q, u, a, r, e) {
+        c.hj=c.hj||function(){(c.hj.q=c.hj.q||[]).push(arguments)};
+        c._hjSettings = { hjid: a };
+        r = s.getElementsByTagName('head')[0];
+        e = s.createElement('script');
+        e.async = true;
+        e.src = q + c._hjSettings.hjid + u;
+        r.appendChild(e);
+    })(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', ${process.env.HOTJAR_ID});`
   }
 };
+
 
 const theme = createTheme({
   fontSizes: {
