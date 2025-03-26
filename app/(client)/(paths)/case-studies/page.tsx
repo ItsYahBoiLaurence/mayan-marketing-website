3316b07a30fa1e0feed3bad44177aa97187e6efb
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-03-26 18:59:18 +0800

    Adjusted the case-studies

--- app/(client)/(paths)/case-studies/page.tsx
+++ app/(client)/(paths)/case-studies/page.tsx
@@ -1,4 +1,4 @@
-import { BackgroundImage, Box, Center, SimpleGrid, Stack, Text } from '@mantine/core'
+import { BackgroundImage, Box, Center, SimpleGrid, Stack, Text, Title } from '@mantine/core'
 import React from 'react'
 import StudyCard from '@/app/(client)/components/Cards/StudyCard'
 import { client } from '@/sanity/lib/client'
@@ -22,28 +22,46 @@ const page = async () => {
     const post = await getPosts()
 
     return (
-        <div>
-            <>
-                <BackgroundImage src='/bannerOverlap.png' w={'100%'} h={'80vh'} mb={'60px'}>
-                    <Center h={'100%'} w={'100%'} bg={'#042161'} opacity={0.95}>
-                        <Stack w={'40%'}>
-                            <Text c={'#fff'} size='xs' tt={'uppercase'} ta={'center'}>Case Studies</Text>
-                            <Text size='48px' fw={700} c={'#fff'} ta={'center'}>Transforming Ideas Into Impactful Success Stories</Text>
-                            <Box w={'60%'} mx={'auto'}>
-                                <Text size='xs' c={'#fff'} ta={'center'}>Every Success starts with a idea. We turn vissions into reality, crafting experiences that inspire action, drive growth, and create lasting impact</Text>
-                            </Box>
-                        </Stack>
-                    </Center>
-                </BackgroundImage>
-                <Box my={'md'} w={'80%'} mx={'auto'}>
-                    <SimpleGrid cols={3} verticalSpacing={'md'}>
-                        {post.map((post: Post) => (
-                            <StudyCard key={post._id} post={post} />
-                        ))}
-                    </SimpleGrid>
-                </Box>
-            </>
-        </div>
+        <>
+            <BackgroundImage src='/bannerOverlap.png' w={'100%'} h={'80vh'} mb={'60px'} visibleFrom='md'>
+                <Center h={'100%'} w={'100%'} bg={'#042161'} opacity={0.95}>
+                    <Stack w={'40%'}>
+                        <Text c={'#fff'} size='xs' tt={'uppercase'} ta={'center'}>Case Studies</Text>
+                        <Text size='48px' fw={700} c={'#fff'} ta={'center'}>Transforming Ideas Into Impactful Success Stories</Text>
+                        <Box w={'60%'} mx={'auto'}>
+                            <Text size='xs' c={'#fff'} ta={'center'}>Every Success starts with a idea. We turn vissions into reality,
+                                crafting experiences that inspire action, drive growth, and create lasting impact</Text>
+                        </Box>
+                    </Stack>
+                </Center>
+            </BackgroundImage>
+            <Box my={'md'} w={'80%'} mx={'auto'} visibleFrom='md'>
+                <SimpleGrid cols={3} verticalSpacing={'md'}>
+                    {post.map((post: Post) => (
+                        <StudyCard key={post._id} post={post} />
+                    ))}
+                </SimpleGrid>
+            </Box>
+
+            <BackgroundImage src='/bannerOverlap.png' w={'100%'} h={'60vh'} mb={'60px'} hiddenFrom='md'>
+                <Center h={'100%'} w={'100%'} bg={'#042161'} opacity={0.95}>
+                    <Stack w={'90%'}>
+                        <Text c={'#fff'} size='xs' tt={'uppercase'}>Case Studies</Text>
+                        <Title order={2} fw={700} c={'#fff'}>Transforming Ideas Into Impactful Success Stories</Title>
+                        <Box w={'100%'} mx={'auto'}>
+                            <Text size='xs' c={'#fff'}>Every Success starts with a idea. We turn vissions into reality, crafting experiences that inspire action, drive growth, and create lasting impact</Text>
+                        </Box>
+                    </Stack>
+                </Center>
+            </BackgroundImage>
+            <Box my={'md'} w={'80%'} mx={'auto'} hiddenFrom='md'>
+                <SimpleGrid cols={3} verticalSpacing={'md'}>
+                    {post.map((post: Post) => (
+                        <StudyCard key={post._id} post={post} />
+                    ))}
+                </SimpleGrid>
+            </Box>
+        </>
     )
 }
 
